291ab403af288aedf433cee4a705fb63a08a2cba
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-08-27 16:46:54 -0300

    H2 database, test profile, JPA

--- src/main/java/com/soterocra/aulapds1/entities/User.java
+++ src/main/java/com/soterocra/aulapds1/entities/User.java
@@ -1,11 +1,18 @@
 package com.soterocra.aulapds1.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = -5625342486695468866L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
