8dcf48f84acaf7018e4b32cb969dab3829243543
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-08-27 17:04:54 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/soterocra/aulapds1/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.soterocra.aulapds1.repositories;
+
+import com.soterocra.aulapds1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
