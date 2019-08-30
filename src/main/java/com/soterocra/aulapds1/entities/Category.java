1d82c824fc1cce20047b32c3fe2b0c2311bb4fef
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-08-30 16:51:55 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/soterocra/aulapds1/entities/Category.java
+++ src/main/java/com/soterocra/aulapds1/entities/Category.java
@@ -1,5 +1,7 @@
 package com.soterocra.aulapds1.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
@@ -14,7 +16,8 @@ public class Category {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
