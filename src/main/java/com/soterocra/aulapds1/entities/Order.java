d9b16b977c6d2ffeb6e3dac5ddd488de91f188fd
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-09-03 19:57:05 -0300

    Subtotal & Total methods

--- src/main/java/com/soterocra/aulapds1/entities/Order.java
+++ src/main/java/com/soterocra/aulapds1/entities/Order.java
@@ -90,6 +90,16 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        Double sum = 0.0;
+
+        for (OrderItem x : getItems()) {
+            sum += x.getSubTotal();
+        }
+
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
