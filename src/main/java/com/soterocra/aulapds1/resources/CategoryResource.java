e656980db89317e6c5a3c33b5fce354d3ebd7450
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-09-15 18:03:27 -0300

    Add complete CRUD of 'categories' endpoint

--- src/main/java/com/soterocra/aulapds1/resources/CategoryResource.java
+++ src/main/java/com/soterocra/aulapds1/resources/CategoryResource.java
@@ -4,11 +4,10 @@ import com.soterocra.aulapds1.entities.Category;
 import com.soterocra.aulapds1.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +26,25 @@ public class CategoryResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+        obj = service.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 
