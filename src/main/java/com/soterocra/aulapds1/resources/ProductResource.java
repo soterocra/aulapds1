c834c70d13904ad6634cda59fe2a4eed41faf769
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-10-08 17:47:06 -0300

    Product insert

--- src/main/java/com/soterocra/aulapds1/resources/ProductResource.java
+++ src/main/java/com/soterocra/aulapds1/resources/ProductResource.java
@@ -1,14 +1,14 @@
 package com.soterocra.aulapds1.resources;
 
+import com.soterocra.aulapds1.dto.ProductCategoriesDTO;
 import com.soterocra.aulapds1.dto.ProductDTO;
 import com.soterocra.aulapds1.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +28,13 @@ public class ProductResource {
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
+        ProductDTO newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
     }
 
 }
--- src/main/java/com/soterocra/aulapds1/services/ProductService.java
+++ src/main/java/com/soterocra/aulapds1/services/ProductService.java
@@ -1,11 +1,16 @@
 package com.soterocra.aulapds1.services;
 
+import com.soterocra.aulapds1.dto.CategoryDTO;
+import com.soterocra.aulapds1.dto.ProductCategoriesDTO;
 import com.soterocra.aulapds1.dto.ProductDTO;
+import com.soterocra.aulapds1.entities.Category;
 import com.soterocra.aulapds1.entities.Product;
+import com.soterocra.aulapds1.repositories.CategoryRepository;
 import com.soterocra.aulapds1.repositories.ProductRepository;
 import com.soterocra.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +22,9 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     public List<ProductDTO> findAll() {
         List<Product> list = repository.findAll();
         return list.stream().map(ProductDTO::new).collect(Collectors.toList());
@@ -26,6 +34,22 @@ public class ProductService {
         Optional<Product> obj = repository.findById(id);
         Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
         return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductCategoriesDTO dto) {
+        Product entity = dto.toEntity();
+        setProductCategories(entity, dto.getCategories());
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+        entity.getCategories().clear();
+        categories.forEach(e -> {
+            Category category = categoryRepository.getOne(e.getId());
+            entity.getCategories().add(category);
+        });
     }
 
 }
