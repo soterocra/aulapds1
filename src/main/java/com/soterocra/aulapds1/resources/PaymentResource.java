35feabd5b832172854ffd39ad2319c9a09a6402c
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-10-18 17:03:13 -0300

    Payment PUT POST endpoints

--- src/main/java/com/soterocra/aulapds1/resources/PaymentResource.java
+++ src/main/java/com/soterocra/aulapds1/resources/PaymentResource.java
@@ -1,15 +1,15 @@
 package com.soterocra.aulapds1.resources;
 
 import com.soterocra.aulapds1.dto.PaymentDTO;
+import com.soterocra.aulapds1.dto.PaymentDTO;
 import com.soterocra.aulapds1.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,6 +30,21 @@ public class PaymentResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<PaymentDTO> findById(@PathVariable Long id) {
         PaymentDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO dto) {
+        PaymentDTO newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO dto) {
+        dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 
--- src/main/java/com/soterocra/aulapds1/services/PaymentService.java
+++ src/main/java/com/soterocra/aulapds1/services/PaymentService.java
@@ -1,16 +1,16 @@
 package com.soterocra.aulapds1.services;
 
-import com.soterocra.aulapds1.dto.CategoryDTO;
 import com.soterocra.aulapds1.dto.PaymentDTO;
-import com.soterocra.aulapds1.entities.Category;
+import com.soterocra.aulapds1.entities.Order;
 import com.soterocra.aulapds1.entities.Payment;
-import com.soterocra.aulapds1.repositories.CategoryRepository;
+import com.soterocra.aulapds1.repositories.OrderRepository;
 import com.soterocra.aulapds1.repositories.PaymentRepository;
-import com.soterocra.aulapds1.repositories.ProductRepository;
 import com.soterocra.aulapds1.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,6 +21,9 @@ public class PaymentService {
     @Autowired
     private PaymentRepository repository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     public List<PaymentDTO> findAll() {
         List<Payment> list = repository.findAll();
         return list.stream().map(PaymentDTO::new).collect(Collectors.toList());
@@ -30,6 +33,31 @@ public class PaymentService {
         Optional<Payment> obj = repository.findById(id);
         Payment entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
         return new PaymentDTO(entity);
+    }
+
+    @Transactional
+    public PaymentDTO insert(PaymentDTO dto) {
+        Order order = orderRepository.getOne(dto.getOrderId());
+        Payment payment = new Payment(null, dto.getMoment(), order);
+        order.setPayment(payment);
+        orderRepository.save(order);
+        return new PaymentDTO(order.getPayment());
+    }
+
+    @Transactional
+    public PaymentDTO update(Long id, PaymentDTO dto) {
+        try {
+            Payment entity = repository.getOne(id);
+            updateData(entity, dto);
+            entity = repository.save(entity);
+            return new PaymentDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Payment entity, PaymentDTO dto) {
+        entity.setMoment(dto.getMoment());
     }
 
 }
