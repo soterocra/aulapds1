51c3e52c0bf45d4498175f91c072109fe42c807d
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-10-18 17:47:42 -0300

    Find products by category

--- src/main/java/com/soterocra/aulapds1/services/ProductService.java
+++ src/main/java/com/soterocra/aulapds1/services/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -89,4 +90,10 @@ public class ProductService {
         }
     }
 
+    @Transactional
+    public Page<ProductDTO> findByCategoryPaged(Long categoryId, Pageable pageable) {
+        Category category = categoryRepository.getOne(categoryId);
+        Page<Product> products = repository.findByCategory(category, pageable);
+        return products.map(ProductDTO::new);
+    }
 }
